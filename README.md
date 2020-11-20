335ed5f088da8c6e301979d035a52031fff7a42f
dizhu <di.zhu.jude@gmail.com>
2020-11-20 14:15:08 +1300

    Add a bit of readme

--- README.md
+++ README.md
@@ -1,1 +1,2 @@
-# gs1-compression
+# GS1 Digital Link Decompression Prototype in Python
+This is a Python translation of the JavaScript Toolkit that decompresses element strings to GS1 digital links.
--- setup.py
+++ setup.py
@@ -15,7 +15,7 @@ setuptools.setup(
     packages=setuptools.find_packages(),
     classifiers=[
         "Programming Language :: Python :: 3",
-        "License :: OSI Approved :: Apache License",
+        "License :: OSI Approved :: Apache License 2.0",
         "Operating System :: OS Independent",
     ],
     python_requires='>=3.6',

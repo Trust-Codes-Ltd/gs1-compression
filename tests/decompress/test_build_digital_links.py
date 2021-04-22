c838c1fb2a983ec3be5d4481709962a3a756e64a
dizhu <di.zhu.jude@gmail.com>
2021-04-23 09:38:06 +1200

    remove all reference to truea2.com

--- tests/decompress/test_build_digital_links.py
+++ tests/decompress/test_build_digital_links.py
@@ -7,9 +7,9 @@ class TestBuildDigitalLink(TestCase):
     """Test case for building GS1 digital link."""
     def test_build_gs1_digital_link(self):
         """Test build_gs1_digital_link."""
-        uri_stem = "https://truea2.com"
+        uri_stem = "https://id.gs1.org"
         array = {"10": "2010005828", "21": "xyz1234", "01": "09421902960055"}
         self.assertEqual(
             build_gs1_digital_link(array, False, uri_stem, {}),
-            'https://truea2.com/01/09421902960055/10/2010005828/21/xyz1234'
+            'https://id.gs1.org/01/09421902960055/10/2010005828/21/xyz1234'
         )

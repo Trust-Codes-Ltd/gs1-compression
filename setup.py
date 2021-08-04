17609b19ed7d199a42a980426a5361e26cae134a
dizhu <di.zhu.jude@gmail.com>
2021-08-04 16:25:05 +1200

    new version

--- setup.py
+++ setup.py
@@ -5,7 +5,7 @@ with open("README.md", "r") as fh:
 
 setuptools.setup(
     name="gs1-compression",
-    version="0.1.2",
+    version="0.1.3",
     author="Di Zhu",
     author_email="di.zhu@trust.codes",
     description=("A Python package to handle compression"
